1ffd153da4d2cc827107af062e6d3151fbda3bc6
Tyler Shepherd <tyman7733@gmail.com>
2020-01-16 18:21:39 -0500

    first commit

---
+++ src/SecondProgram.java
@@ -0,0 +1,11 @@
+
+public class SecondProgram {
+	public static void main(String[] args) {
+		
+		int age = 12;
+		double weight = 25.3649;
+		System.out.printf("Hello Everyone!\nI am %d years old!\nI weight %.2f", age, weight);
+
+	}
+
+}
